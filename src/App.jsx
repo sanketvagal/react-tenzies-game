1ce806e48eab3593c401717e36d088aa3b5ad5ca
sanketvagal <sanketdvagal@gmail.com>
2023-07-17 18:18:51 -0300

    Added confetti when player wins :D

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Die from "./Die"
 import { nanoid } from "nanoid"
+import Confetti from 'react-confetti'
 
 export default function App() {
 
@@ -54,6 +55,7 @@ export default function App() {
 
   return (
     <main>
+      {tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
